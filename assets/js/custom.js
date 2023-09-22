e61f8f60bf69c2872c5e8d3fcf5d563278c44c8b
pixerize <40294788+pixerize@users.noreply.github.com>
2023-09-22 18:02:23 +0000

    Deploying to gh-pages from @ rs-statistika-sdg/sdg-site-rs@1b67c8a076b10966e216f91e8da035a30fe8e636 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
   $("a[href*='/1-a-3']").parent().remove();
-  $("a[href*='/8-9-2']").parent().remove();
   $("a[href*='/13-3-2']").parent().remove();
   $("a[href*='/17-6-2']").parent().remove();
 });
